7241e74b993bb816c1780d3a940342c61cc22ee9
MetinAgaoglu <metnagaoglu@gmail.com>
2016-05-04 17:14:51 +0300

    V3-Maps Eklendi

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,22 +2,30 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-
-})
-.controller('SearchCtrl', function($scope, $state) {
-  $scope.submit = function(par){
-    $state.go('app.detay', {par: par});
-  }
-})
-.controller('DetayCtrl', function($scope, $stateParams, $http){
-  $http.get('https://api.foursquare.com/v2/venues/search?query='+$stateParams.par+'&near=Bursa&client_id=TLJRWP3BKS20TVNO3JDV1PHT1XZYJRXCDWK4VKZZY2VTQNH2&client_secret=IACQVEXQWYCYJDOFCKXBTRLGIPZJ3CGZULDCQC2CXGZW2TFP&v=20160502')
-  .success(function(response){
-    $scope.venues = response.response.venues;
-  });
-})
-.controller('ResultCtrl', function($scope, $stateParams, $http){
-  $http.get('https://api.foursquare.com/v2/venues/'+ $stateParams.id +'/?client_id=TLJRWP3BKS20TVNO3JDV1PHT1XZYJRXCDWK4VKZZY2VTQNH2&client_secret=IACQVEXQWYCYJDOFCKXBTRLGIPZJ3CGZULDCQC2CXGZW2TFP&v=20160502')
-  .success(function(response){
-    $scope.detay = response.response.venue;
   })
-});
+  .controller('SearchCtrl', function($scope, $state) {
+    $scope.submit = function(par) {
+      $state.go('app.detay', {
+        par: par
+      });
+    }
+  })
+  .controller('DetayCtrl', function($scope, $stateParams, $http) {
+    $http.get('https://api.foursquare.com/v2/venues/search?query=' + $stateParams.par + '&near=Bursa&client_id=TLJRWP3BKS20TVNO3JDV1PHT1XZYJRXCDWK4VKZZY2VTQNH2&client_secret=IACQVEXQWYCYJDOFCKXBTRLGIPZJ3CGZULDCQC2CXGZW2TFP&v=20160502')
+      .success(function(response) {
+        $scope.venues = response.response.venues;
+      });
+  })
+  .controller('ResultCtrl', function($scope, $stateParams, $http) {
+    $http.get('https://api.foursquare.com/v2/venues/' + $stateParams.id + '/?client_id=TLJRWP3BKS20TVNO3JDV1PHT1XZYJRXCDWK4VKZZY2VTQNH2&client_secret=IACQVEXQWYCYJDOFCKXBTRLGIPZJ3CGZULDCQC2CXGZW2TFP&v=20160502')
+      .success(function(data) {
+        $scope.detay = data.response.venue;
+        $scope.resim = data.response.venue.bestPhoto;
+      });
+    $scope.map = function() {
+      alert("Yükleniyor..");
+      var map = new GMap2(document.getElementById("map"));
+      map.setCenter(new GLatLng($scope.detay.location.lat, $scope.detay.location.lng), 18);
+    }
+
+  });
